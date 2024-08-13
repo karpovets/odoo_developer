87c7ec85cd44edc3a0f8bf14a76b1ad24ee7efce
Karpovets Ivan <karpovets90@gmail.com>
2024-08-13 13:17:10 +0000

    chapter_2, створив модуль, додав файл опису модуля

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real Estate",
+    'author': "Karpovets Ivan",
+    'category': 'Custom',
+    'description': "My new module",
+    'version': '17.1',
+    'depends': [
+        'base'
+    ],
+    'data': [],
+    'application': True,
+    'installable': True
+}
