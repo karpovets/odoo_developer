2cd8fe52d8522d9670368fdae05763042a334449
Karpovets Ivan <karpovets90@gmail.com>
2024-08-19 20:22:23 +0000

    chapter 7, додавання нових сутностей, створення меню, звязка сутностей між собою, додавання полів в редагування

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -35,3 +35,7 @@ class EstateProperty(models.Model):
             ('sold', 'Sold'),
             ('canceled', 'Canceled')
         ], required=1, copy=False, default='new')
+    property_type_id = fields.Many2one('estate.property.type', string="Property Type")
+    user_id = fields.Many2one('res.users', string='Salesperson', copy=False, default=lambda self: self.env.user)
+    tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id')
