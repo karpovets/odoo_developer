da6942a8295d1009839e0431e2b48dd1d777b12b
Karpovets Ivan <karpovets90@gmail.com>
2024-09-23 20:07:46 +0000

    Заборонити змінювати час/дату/лікаря прийому, що відбувся, Заборонити видаляти або архівувати відвідування з діагнозами

--- hospital/models/hospital_visiting_doctor.py
+++ hospital/models/hospital_visiting_doctor.py
@@ -21,7 +21,7 @@ class HospitalVisitingDoctor(models.Model):
         ('check_time', 'CHECK(start_time < end_time)', 'The start time must be earlier than the end time.')
     ]
     
-    @api.constrains('doctor', 'visiting_date', 'start_time', 'end_time')
+    @api.constrains('doctor', 'visiting_date', 'start_time', 'end_time', 'active')
     def _check_time_overlap(self):
         for record in self:
             overlapping_visit = self.env['hospital.visiting.doctor'].search([
@@ -33,3 +33,27 @@ class HospitalVisitingDoctor(models.Model):
             ])
             if overlapping_visit:
                 raise ValidationError(f"The doctor {record.doctor.full_name} is already booked for this time on {record.visiting_date}. Please choose another time.")
+            
+
+    def _check_appointment_editable(self):
+        for record in self:
+            if record.visit_occurred == True:
+                raise ValidationError("Ви не можете змінювати час, дату або лікаря прийому, який вже відбувся.")
+
+    def write(self, vals):
+        for record in self:
+            if record.visit_occurred == True:
+                if 'visiting_date' in vals or 'doctor' in vals:
+                    raise ValidationError("Ви не можете змінювати час, дату або лікаря прийому, який вже відбувся.")
+        return super(HospitalVisitingDoctor, self).write(vals)
+    
+    def _check_archiving(self):
+        for record in self:
+            if not record.active and record.diseases:
+                raise ValidationError("Ви не можете архівувати відвідування, яке має діагнози.")
+
+    def unlink(self):
+        for record in self:
+            if record.diseases:
+                raise ValidationError("Ви не можете видаляти відвідування, яке має діагнози.")
+        return super(HospitalVisitingDoctor, self).unlink()
